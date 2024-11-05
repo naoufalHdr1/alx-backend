24c37ba1811badb8954645bae823c745fa5b9e51
naoufalHdr1 <naoufalhadra96@gmail.com>
2024-11-05 18:29:44 +0100

    Task 2: Get locale from request

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -3,7 +3,7 @@
 Flask application with basic Babel setup for internationalization (i18n).
 Supports English and French locales.
 """
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
@@ -22,12 +22,21 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
+@babel.localeselector
+def get_locale() -> str:
+    """
+    Determines the best match for supported languages based on the
+    Accept-Language headers in the incoming request.
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
 @app.route("/")
 def index() -> str:
     """
     Renders the index page with a welcome message.
     """
-    return render_template('1-index.html')
+    return render_template('2-index.html')
 
 
 if __name__ == "__main__":
