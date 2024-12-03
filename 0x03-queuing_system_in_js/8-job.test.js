7e5db0d1974574fc82c4fc7f631d3e7916797496
naoufalHdr1 <naoufalhadra96@gmail.com>
2024-12-03 20:28:00 +0100

    Task 11: update file to test more cases

--- 0x03-queuing_system_in_js/8-job.test.js
+++ 0x03-queuing_system_in_js/8-job.test.js
@@ -1,22 +1,26 @@
 import { expect } from 'chai';
 import kue from 'kue';
+import sinon from 'sinon';
 import createPushNotificationsJobs from './8-job.js';
 
-describe('createPushNotificationsJobs', () => {
+describe('createPushNotificationsJobs', function () {
     let queue;
+    let logSpy;
 
     beforeEach(() => {
         queue = kue.createQueue();
-        queue.testMode.enter();
+	    queue.testMode.enter();
+        logSpy = sinon.spy(console, 'log');         // Spy on console.log
     });
 
     afterEach(() => {
-        queue.testMode.clear();
-        queue.testMode.exit();
+        queue.testMode.clear(); // Clear the test queue
+        queue.testMode.exit();  // Exit test mode
+        sinon.restore();        // Restore spied methods
     });
 
     it('should display an error message if jobs is not an array', () => {
-        expect(() => createPushNotificationsJobs(123, queue)).to.throw('Jobs is not an array');
+        expect(() => createPushNotificationsJobs(42, queue)).to.throw('Jobs is not an array');
     });
 
     it('should create two new jobs to the queue', () => {
@@ -27,19 +31,17 @@ describe('createPushNotificationsJobs', () => {
             },
             {
                 phoneNumber: '4153518781',
-                message: 'This is the code 5678 to verify your account',
+                message: 'This is the code 4321 to verify your account',
             },
         ];
 
         createPushNotificationsJobs(jobs, queue);
-
-        expect(queue.testMode.jobs.length).to.equal(2);
+        expect(queue.testMode.jobs).to.have.lengthOf(2); // Correct check
         expect(queue.testMode.jobs[0].type).to.equal('push_notification_code_3');
-        expect(queue.testMode.jobs[0].data).to.deep.equal(jobs[0]);
-        expect(queue.testMode.jobs[1].data).to.deep.equal(jobs[1]);
+        expect(queue.testMode.jobs[1].type).to.equal('push_notification_code_3');
     });
 
-    it('should log when a job is created', (done) => {
+    it('should handle job events (complete, failed, progress)', (done) => {
         const jobs = [
             {
                 phoneNumber: '4153518780',
@@ -47,19 +49,28 @@ describe('createPushNotificationsJobs', () => {
             },
         ];
 
-        const spy = (msg) => {
-            expect(msg).to.contain('Notification job created:');
-            done();
-        };
-
-        // Mock console.log
-        const originalLog = console.log;
-        console.log = spy;
-
         createPushNotificationsJobs(jobs, queue);
 
-        console.log = originalLog;
-	done();
+        // Get the first job from testMode.jobs
+        const job = queue.testMode.jobs[0];
+
+        // Manually trigger events on the job object
+        setTimeout(() => {
+            try {
+                job.emit('progress', 50);
+                expect(logSpy.calledWithMatch(/Notification job .* 50% complete/)).to.be.true;
+
+                job.emit('complete');
+                expect(logSpy.calledWithMatch(/Notification job .* completed/)).to.be.true;
+
+                job.emit('failed', new Error('Test Error'));
+                expect(logSpy.calledWithMatch(/Notification job .* failed: Test Error/)).to.be.true;
+
+                done();
+            } catch (error) {
+                done(error);
+            }
+        }, 50);
     });
 });
 
